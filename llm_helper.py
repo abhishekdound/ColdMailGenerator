d7bdb5f08f9dc26d8291e64c7e24d61e70c2b716
Abhishek <abhishekdound@gmail.com>
2026-02-17 17:20:01 +0530

    changes made and bug fixed

--- llm_helper.py
+++ llm_helper.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0,  model="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(temperature=0,  model="llama-3.3-70b-versatile")
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
@@ -39,14 +39,14 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at DonUNit. DonUNit is an AI & Software Consulting company dedicated to facilitating
+            You are Dhanush, a business development executive at DonUNit. DonUNit is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
             Your job is to write a cold email to the client regarding the job mentioned above describing the capability of DonUNit 
             in fulfilling their needs.
             Also add the most relevant ones from the following links to showcase DonUNit's portfolio: {link_list}
-            Remember you are Mohan, BDE at DonUNit. 
+            Remember you are Dhanush, BDE at DonUNit. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
 
